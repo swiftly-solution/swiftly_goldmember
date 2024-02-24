68c94e330a08f57177a33f65b0f8b4665d246875
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2024-02-24 20:53:40 +0200

    Added "if PistolRound".
    This will make the plugin to ignore the benefits at the first and the half round. 🫡

--- swiftly_goldmember/game-events/onplayerspawn.lua
+++ swiftly_goldmember/game-events/onplayerspawn.lua
@@ -2,61 +2,64 @@ function OnPlayerSpawnEvent()
     events:on("OnPlayerSpawn", function(playerid)
         local player = GetPlayer(playerid)
         if not player then return end
+        local PistolRound = server:IsPistolRound()
 
-        if HasDNS(player) then
-            NextTick(function()
-                player:health():Set(config:Fetch("goldmember.Benefits.HP"))
-                player:armor():Set(config:Fetch("goldmember.Benefits.Armor"))
-            end)
-            player:money():Set(player:money():Get() + config:Fetch("goldmember.Benefits.Money"))
+        if PistolRound == 0 then
+            if HasDNS(player) then
+                NextTick(function()
+                    player:health():Set(config:Fetch("goldmember.Benefits.HP"))
+                    player:armor():Set(config:Fetch("goldmember.Benefits.Armor"))
+                end)
+                player:money():Set(player:money():Get() + config:Fetch("goldmember.Benefits.Money"))
 
-            local givedefusekit = config:Fetch("goldmember.Benefits.DefuseKit")
+                local GiveDefuseKit = config:Fetch("goldmember.Benefits.DefuseKit")
 
-            if givedefusekit == 1 then
-                local team = player:team():Get()
-                if team == TEAM_CT then
-                    player:weapons():GiveWeapons("item_defuser")
-                end
-            end
-            
-            local givehealthshots = config:Fetch("goldmember.Benefits.HealthShot")
-            
-            for i = 1, givehealthshots do
-                player:weapons():GiveWeapons("weapon_healthshot")
-            end
-
-            local givegrenades = config:Fetch("goldmember.Benefits.GoldMemberGrenades.GiveGreandes")
-
-            if givegrenades == 1 then
-                local hegrenades = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.HighExplosive")
-                local smokegrenade = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Smoke")
-                local flashbang = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Flashbang")
-                local molotovincendiary = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Molotov_Incendiary")
-                local decoy = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Decoy")
-
-                if hegrenades == 1 then
-                    player:weapons():GiveWeapons("weapon_hegrenade")
-                end
-                if smokegrenade == 1 then
-                    player:weapons():GiveWeapons("weapon_smokegrenade")
-                end
-                for i = 1, flashbang do
-                    player:weapons():GiveWeapons("weapon_flashbang")
-                end
-                if molotovincendiary == 1 then
+                if GiveDefuseKit == 1 then
                     local team = player:team():Get()
                     if team == TEAM_CT then
-                        player:weapons():GiveWeapons("weapon_incgrenade")
-                    elseif team == TEAM_T then
-                        player:weapons():GiveWeapons("weapon_molotov")
+                        player:weapons():GiveWeapons("item_defuser")
                     end
                 end
-                if decoy == 1 then
-                    player:weapons():GiveWeapons("weapon_decoy")
-                end
-            end
 
-            local givescoreboardtag = config:Fetch("goldmember.Benefits.GoldMemberTag.GiveScoreboardTag")
+                local GiveHealthShots = config:Fetch("goldmember.Benefits.HealthShot")
+
+                for i = 1, GiveHealthShots do
+                    player:weapons():GiveWeapons("weapon_healthshot")
+                end
+
+                local GiveGrenades = config:Fetch("goldmember.Benefits.GoldMemberGrenades.GiveGreandes")
+
+                if GiveGrenades == 1 then
+                    local hegrenades = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.HighExplosive")
+                    local smokegrenade = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Smoke")
+                    local flashbang = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Flashbang")
+                    local molotovincendiary = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Molotov_Incendiary")
+                    local decoy = config:Fetch("goldmember.Benefits.GoldMemberGrenades.Grenades.Decoy")
+
+                    if hegrenades == 1 then
+                        player:weapons():GiveWeapons("weapon_hegrenade")
+                    end
+                    if smokegrenade == 1 then
+                        player:weapons():GiveWeapons("weapon_smokegrenade")
+                    end
+                    for i = 1, flashbang do
+                        player:weapons():GiveWeapons("weapon_flashbang")
+                    end
+                    if molotovincendiary == 1 then
+                        local team = player:team():Get()
+                        if team == TEAM_CT then
+                            player:weapons():GiveWeapons("weapon_incgrenade")
+                        elseif team == TEAM_T then
+                            player:weapons():GiveWeapons("weapon_molotov")
+                        end
+                    end
+                    if decoy == 1 then
+                        player:weapons():GiveWeapons("weapon_decoy")
+                    end
+                end
+
+                local GiveScoreboardTag = config:Fetch("goldmember.Benefits.GoldMemberTag.GiveScoreboardTag")
+            end
         end
     end)
 end
--- swiftly_goldmember/game-events/onroundstart.lua
+++ swiftly_goldmember/game-events/onroundstart.lua
@@ -1,16 +1,19 @@
 function OnRoundStartEvent()
     events:on("OnRoundStart", function(timelimit, fraglimit, objective)
-        for playerid = 0, playermanager:GetPlayerCount() -1, 1 do
+        local PistolRound = server:IsPistolRound()
+
+        for playerid = 0, playermanager:GetPlayerCap() - 1, 1 do
             local player = GetPlayer(playerid)
-            if not player then goto OnRoundStartcontinue end
-            
-            if HasDNS(player) then
-                serverDNS = config:Fetch("goldmember.ServerDNS")
-                player:SendMsg(MessageType.Chat, string.format(config:Fetch("goldmember.Messages.HasGoldMember"), serverDNS))
-            else
-                player:SendMsg(MessageType.Chat, config:Fetch("goldmember.Messages.NoGoldMember"))
+            if player then
+                if PistolRound ~= 0 and HasDNS(player) then
+                    player:SendMsg(MessageType.Chat, config:Fetch("goldmember.Messages.DisabledGoldMember"))
+                elseif not HasDNS(player) then
+                    local ServerDNS = config:Fetch("goldmember.ServerDNS")
+                    player:SendMsg(MessageType.Chat, string.format(config:Fetch("goldmember.Messages.NoGoldMember"), ServerDNS))
+                else
+                    player:SendMsg(MessageType.Chat, config:Fetch("goldmember.Messages.HasGoldMember"))
+                end
             end
-            ::OnRoundStartcontinue::
         end
     end)
 end
